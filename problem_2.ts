aba1461f8d2ce6388e542dda6173e3174f91075e
Jirawat Phattarawasitpol <jirawat.phattarawasitpol@gmail.com>
2024-06-09 02:58:10 +0700

    fix problem 2

--- problem_2.ts
+++ problem_2.ts
@@ -1,3 +1,9 @@
+type Data = {
+    nameStr: string;
+    digits: number;
+    otherStr: string;
+};
+
 const getNumber = (str: string): number => {
     let match = str.match(/\d+/);
 
@@ -8,14 +14,35 @@ const getNumber = (str: string): number => {
     return parseInt(match[0]);
 };
 
-function sortFunction(arr: string[]): string[] {
-    return arr.sort((a, b) => {
-        let na = getNumber(a);
-        let nb = getNumber(b);
+const sortFunction = (arr: string[]): string[] => {
+    let data: Data[] = [];
 
-        return na - nb;
+    for (let index = 0; index < arr.length; index++) {
+        let temp = {
+            nameStr: arr[index].slice(0, 2),
+            digits: getNumber(arr[index]),
+            otherStr: arr[index].replace(arr[index].slice(0, 2), "").replace(getNumber(arr[index]).toString(), ""),
+        };
+
+        data = [...data, temp];
+    }
+
+    data.sort((a, b) => {
+        return a.nameStr.localeCompare(b.nameStr) || a.digits - b.digits;
     });
-}
+
+    let result: string[] = [];
+
+    for (let index = 0; index < data.length; index++) {
+        const dataWithIndex = data[index];
+
+        const combineOb = dataWithIndex.nameStr.concat(dataWithIndex.digits.toString()).concat(dataWithIndex.otherStr);
+
+        result = [...result, combineOb];
+    }
+
+    return result;
+};
 
 console.log(sortFunction(["TH19", "SG20", "TH2"]));
 console.log(sortFunction(["TH10", "TH3Netflix", "TH1", "TH7"]));
